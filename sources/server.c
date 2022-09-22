8710f6dca34bf74c38b7f837bff0d59df91dc632
Clementine CHEYROU-LAGREZE <ccheyrou@e1r11p14.clusters.42paris.fr>
2022-09-22 18:49:14 +0200

    [ADD] signal to string

--- sources/server.c
+++ sources/server.c
@@ -12,6 +12,43 @@ void	handle_sigusr2(int sig)
 	printf("0\n");
 }
 
+char	*signals_to_strings(char *str)
+{
+    int     size;
+    int        i;
+    int        j;
+	int		res;
+	char		*s;
+	int		l;
+
+    if (str == NULL)
+        return (NULL);
+    size = ft_strlen(str);
+	s = malloc(sizeof(char) * size / 8);
+	if (!s)
+		return (NULL);
+    i = 0;
+	l = 0;
+    while (i < size)
+    {
+		res = 0;
+		j = 7;
+        while (j >= 0)
+        {
+			if (str[i] == 49)
+				res = res * 2 + 1;
+			else
+				res *= 2 ;
+			j--;
+			i++;
+		}
+		s[l] = res;
+		l++;
+	}
+	s[l] = '\0';
+	return (s);
+}
+
 int	main(int argc, char **argv)
 {
 	int	my_pid;
